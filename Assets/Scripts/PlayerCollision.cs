95601899d2cc52184c46117b474c10859e05dbcb
Amine Aboufariss <farissamine99@gmail.com>
2020-08-25 16:45:21 -0500

    First commit

---
+++ Assets/Scripts/PlayerCollision.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+
+    public PlayerMovement movement;
+
+    void OnCollisionEnter( Collision collisionInfo)
+    {
+        Debug.Log( collisionInfo.collider.name);
+
+        if(collisionInfo.collider.tag == "Ground")
+        {
+            Debug.Log("We hit the Ground");
+            if (!movement.hasJump())
+            {
+                movement.setJumpAvailable(true);
+            }
+        }
+
+        if(collisionInfo.collider.tag == "Obstacle")
+        {
+            Debug.Log("We hit an obstacle");
+
+            movement.enabled = false;
+        }
+    }   
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,119 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public Rigidbody rb;
+
+    public float forwardForce = 600f;
+    //public float backwardsForce = 600f;
+    public float sidewaysForce = 100f;
+    public float jumpForce = 500f;
+    private bool moveForwards = true;
+    private bool moveBackwards = false;
+    private bool moveRight = false;
+    private bool moveLeft = false;
+    private bool jumpAvailable = true;
+    private bool jumping = false;
+
+    ForceMode fm = ForceMode.VelocityChange;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Debug.Log("Start Occurred");
+        //rb.AddForce(0,200, 500);
+    }
+
+
+    void Update()
+    {
+
+        if (Input.GetKeyDown("w"))
+        {
+            //moveForwards = true;
+        }
+        if (Input.GetKeyUp("w"))
+        {
+            //moveForwards = false;
+        }
+
+
+        if (Input.GetKeyDown("s"))
+        {
+            //moveBackwards = true;
+        }
+        if (Input.GetKeyUp("s"))
+        {
+           //moveBackwards = false;
+        }
+
+
+
+        if (Input.GetKeyDown("d"))
+        {
+            moveRight = true;
+        }
+        if (Input.GetKeyUp("d"))
+        {
+            moveRight = false;
+        }
+
+
+        if (Input.GetKeyDown("a"))
+        {
+            moveLeft = true;
+        }
+        if (Input.GetKeyUp("a"))
+        {
+            moveLeft = false;
+        }
+
+
+        if (Input.GetKeyDown("space"))
+        {
+            jumping = true;
+           
+            
+        }
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()//FixedUpdate better for physics
+    {
+        if (moveForwards)
+        {
+            rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+        }
+        if (moveBackwards)
+        {
+            rb.AddForce(0, 0, -forwardForce * Time.deltaTime);
+        }
+
+        if (moveRight)
+        {
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0, fm);
+        }
+        if (moveLeft)
+        {
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0, fm);
+        }
+        if (jumping)
+        {
+            if (jumpAvailable) { jump(); }
+        }
+        
+    }
+
+
+    public bool hasJump(){return jumpAvailable;}
+    public void setJumpAvailable(bool j) { jumpAvailable = j; }
+
+    private void jump() {
+        rb.AddForce(0, jumpForce, 0);
+        jumping = false;
+        jumpAvailable = false;
+    }
+}
